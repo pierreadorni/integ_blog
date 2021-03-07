4cb8edba8c2022a67766e706a7ba2b3b11f775bb
pierreadorni <pierreadorni@free.fr>
2021-03-07 20:58:22 +0100

    fixed connection

--- connect.php
+++ connect.php
@@ -1,23 +1,26 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
+
+    <meta charset='utf-8'></meta>
+    <title>Connexion à la page de modification</title>
+    <link rel='stylesheet' href="src/style.css"></link>
     <?php
-        $team = $_GET['team'];
+        $team = $_GET['t'];
         $wrong_pass = false;
         if (isset($_POST['pass'])) {
             if (hash('md5', $_POST['pass']) == '3a8e8b3c91150fded1e49ef03639b992') {
-                echo '<meta http-equiv = "refresh" content = "0; url=modif.php?t='.$team.'" />';
+                echo '<meta http-equiv = "refresh" content = "0; url = modif.php?t='.$team.'" />';
             } else {
                 $wrong_pass = true;
             }
         }
     ?>
-    <meta charset='utf-8'></meta>
-    <title>Connexion à la page de modification</title>
-    <link rel='stylesheet' href="src/style.css"></link>
 </head>
 <body>
-    <form action='connect.php' method='post'>
+    <form action='connect.php?t=<?php echo $team;?>' method='post'>
         <?php
         if ($wrong_pass) {
             echo '<p><b>MAUVAIS MOT DE PASSE.</b></p>';
